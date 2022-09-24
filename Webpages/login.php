17aa80f3f31934c0f1517b369407dfd90135263f
MalcolmS3160 <mcshuler121212@gmail.com>
2022-09-24 13:28:05 -0400

    Created login page, student page, and teacher page

---
+++ Webpages/login.php
@@ -0,0 +1,16 @@
+<HTML>
+    <Head>
+         <Title>Login Page</Title>
+    </Head>
+
+    <Body>
+        <Form Name = "loginForm" Method = "post" Action = "validate.php">
+            <label for="user">Username:</label><br>
+            <input type="text" id="user" name="user"><br>
+            <label for="password">Password:</label><br>
+            <input type="password" id="password" name="password"><br>
+            <input type="submit" name="submit" value="Submit">
+        </Form>
+    </Body>
+    
+</HTML>
