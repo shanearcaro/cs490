4cd1dac06f9816f7b1cd30e427bddeeaef19c332
egeatay <ege@atay.us>
2022-09-27 02:52:41 -0400

    updated db fields

--- sql/01.sql
+++ sql/01.sql
@@ -11,18 +11,19 @@ CREATE TABLE `Users` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `username` varchar(32) NOT NULL,
     `password` VARCHAR(99) NOT NULL,
+    `isteacher` BIT NOT NULL DEFAULT 0,
     `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `modified` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
 );
 
-INSERT INTO Users(id, username, password) VALUES(0, "testin", "123456");
+INSERT INTO Users(username, password, isteacher) VALUES("testin", "123456", 1);
 
 
 ALTER table Users2 
 ADD COLUMN username varchar(15) NOT NULL;
 
-DROP TABLE `Users2`;
+DROP TABLE `Users`;
 DROP TABLE `test`;
 
 
