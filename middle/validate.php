9dd6d270e15cb6aa8552bc69314d052ce210ac50
shanearcaro <smarcaro@gmail.com>
2022-09-27 13:20:56 -0400

    Updated response

--- middle/validate.php
+++ middle/validate.php
@@ -24,7 +24,7 @@
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
     // curl_setopt($ch, CURLOPT_HEADER, true);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $encoded);
 
