8d60caad56edf66023f05d8fc948de9e820e3c28
shanearcaro <smarcaro@gmail.com>
2022-09-28 14:32:20 -0400

    Adding better comments for more readable code

--- backend/credentials.php
+++ backend/credentials.php
@@ -1,6 +1,6 @@
 <?php
     $server = "sql1.njit.edu";
     $username = "sma237";
-    $password = "***REMOVED***";
+    $password = "*password*";
     $database = "sma237";
 ?>
--- frontend/login.php
+++ frontend/login.php
@@ -4,6 +4,7 @@
          <title>Login Page</title>
     </head>
     <body>
+        <!-- Need to use send.php to cURL the information instead of standards post -->
         <form name = "loginForm" method = "post" action = "send.php">
             <label for="username">Username:</label><br>
             <input type="text" id="username" name="username"><br>
