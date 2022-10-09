2c3e43fd18ac163447db4e86a39ae636bd0794cd
shanearcaro <smarcaro@gmail.com>
2022-10-09 14:26:47 -0400

    Please work

--- tests/Post/DatabaseTest.php
+++ tests/Post/DatabaseTest.php
@@ -3,7 +3,7 @@ use PHPUnit\Framework\TestCase;
 
 class Database extends TestCase {
     public function test_env_data(): void {
-        $server = getenv('SERVER', true);
+        $server = getenv('SERVER', false);
         $this->assertEquals($server, "sql1.njit.edu");
     }
 }
