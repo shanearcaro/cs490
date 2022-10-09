04288b80175ad60d2447e912efe8994edb06d25a
shanearcaro <smarcaro@gmail.com>
2022-10-09 14:20:46 -0400

    Stupid mistake

--- tests/Post/DatabaseTest.php
+++ tests/Post/DatabaseTest.php
@@ -4,8 +4,6 @@ use PHPUnit\Framework\TestCase;
 class Database extends TestCase {
     public function test_env_data(): void {
         $server = getenv('SERVER', true);
-        $dotenv->load();
-    
         $this->assertEquals($server, "sql1.njit.edu");
     }
 }
