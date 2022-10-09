46087444bdbae575788beb547ee02b6cd6662179
shanearcaro <smarcaro@gmail.com>
2022-10-09 14:30:24 -0400

    Trying fix found online

--- tests/Post/DatabaseTest.php
+++ tests/Post/DatabaseTest.php
@@ -1,10 +1,13 @@
 <?php
 use PHPUnit\Framework\TestCase;
+require_once realpath(__DIR__ . '/vendor/autoload.php');
 
 class Database extends TestCase {
     public function test_env_data(): void {
-        $server = getenv('SERVER', false);
-        $this->assertEquals($server, "sql1.njit.edu");
+        // Looing for .env at the root directory
+        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+        $dotenv->load();
+        $this->assertEquals($_ENV['SERVER'], "sql1.njit.edu");
     }
 }
 ?>
