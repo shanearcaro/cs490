0469d1878a8710cc7ab8823fd30899f414f163cd
shanearcaro <smarcaro@gmail.com>
2022-10-09 15:08:38 -0400

    Fixing real path issue

--- tests/Post/DatabaseTest.php
+++ tests/Post/DatabaseTest.php
@@ -1,11 +1,11 @@
 <?php
 use PHPUnit\Framework\TestCase;
-require_once realpath('../../vendor/autoload.php');
+require_once realpath(dirname(__DIR__, 2) . '/vendor/autoload.php');
 
 class Database extends TestCase {
     public function test_env_data(): void {
         // Looing for .env at the root directory
-        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+        $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 2));
         $dotenv->load();
         $this->assertEquals($_ENV['SERVER'], "sql1.njit.edu");
     }
