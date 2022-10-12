4ad163c057c8dc828296018b315bbb4d379b3e57
Shane Arcaro <smarcaro@gmail.com>
2022-10-12 10:30:00 -0400

    Can't get docker working in time, going to stop using it and focus on finishing this project first

--- create-db.sql
+++ create-db.sql
@@ -6,13 +6,13 @@ USE CS490;
 
 DROP TABLE IF EXISTS UserAccounts
 
-CREATE TABLE UserAccounts (
-    id int(10) NOT NULL AUTO_INCREMENT,
-    username varchar(20) NOT NULL,
-    pass text(255) NOT NULL,
-    isTeacher tinyint(1), NOT NULL,
-    PRIMARY KEY(id)
-);
+-- CREATE TABLE UserAccounts (
+--     id int(10) NOT NULL AUTO_INCREMENT,
+--     username varchar(20) NOT NULL,
+--     pass text(255) NOT NULL,
+--     isTeacher tinyint(1), NOT NULL,
+--     PRIMARY KEY(id)
+-- );
 
-INSERT INTO UserAccounts VALUES('shanearcaro', 'test', 0);
+-- INSERT INTO UserAccounts VALUES('shanearcaro', 'test', 0);
 
--- tests/Post/DatabaseTest.php
+++ tests/Post/DatabaseTest.php
@@ -3,33 +3,33 @@ use PHPUnit\Framework\TestCase;
 require_once realpath(dirname(__DIR__, 2) . '/vendor/autoload.php');
 
 class Database extends TestCase {
-    public function test_env_server(): void {
+    // public function test_env_server(): void {
         // Looing for .env at the root directory
-        $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 2));
-        $dotenv->load();
-        $this->assertEquals($_ENV['SERVER'], "sql1.njit.edu");
-    }
+        // $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 2));
+        // $dotenv->load();
+        // $this->assertEquals($_ENV['SERVER'], "sql1.njit.edu");
+    // }
 
-    public function test_env_username(): void {
+    // public function test_env_username(): void {
         // Looing for .env at the root directory
-        $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 2));
-        $dotenv->load();
-        $this->assertEquals($_ENV['USERNAME'], "sma237");
-    }
+        // $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 2));
+        // $dotenv->load();
+        // $this->assertEquals($_ENV['USERNAME'], "sma237");
+    // }
 
-    public function test_env_database(): void {
+    // public function test_env_database(): void {
         // Looing for .env at the root directory
-        $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 2));
-        $dotenv->load();
-        $this->assertEquals($_ENV['DATABASE'], "sma237");
-    }
+        // $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 2));
+        // $dotenv->load();
+        // $this->assertEquals($_ENV['DATABASE'], "sma237");
+    // }
 
     public function test_database_connection(): void {
         // Looing for .env at the root directory
         $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 2));
         $dotenv->load();
 
-        $connection = mysqli_connect($_ENV['SERVER'], $_ENV['USERNAME'], $_ENV['PASSWORD'], $_ENV['DATABASE']);
+        $connection = mysqli_connect("localhost", "shane", "password");
 
         $this->assertEquals(mysqli_connect_error, null);
     }
