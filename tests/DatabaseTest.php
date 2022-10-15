02150df3fedcea3ab7fb8ea700d3cc0cef38e416
shanearcaro <smarcaro@gmail.com>
2022-10-15 19:53:52 -0400

    Trying error tests again

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -10,12 +10,12 @@ class Database extends TestCase {
         $this->assertEquals($_ENV['HOST'], "db");
     }
 
-    // public function test_env_username(): void {
-    //     // Looing for .env at the root directory
-    //     $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 1));
-    //     $dotenv->load();
-    //     $this->assertEquals($_ENV['USER'], "user");
-    // }
+    public function test_env_username(): void {
+        // Looing for .env at the root directory
+        $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 1));
+        $dotenv->load();
+        $this->assertEquals($_ENV['USER'], "user");
+    }
 
     public function test_env_database(): void {
         // Looing for .env at the root directory
@@ -24,14 +24,14 @@ class Database extends TestCase {
         $this->assertEquals($_ENV['DATABASE'], "sma237");
     }
 
-    // public function test_database_connection(): void {
-    //     // Looing for .env at the root directory
-    //     $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 1));
-    //     $dotenv->load();
+    public function test_database_connection(): void {
+        // Looing for .env at the root directory
+        $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 1));
+        $dotenv->load();
 
-    //     $connection = new mysqli($_ENV['HOST'], $_ENV['USER'], $_ENV['PASSWORD'], $_ENV['DATABASE']);
+        $connection = new mysqli($_ENV['HOST'], $_ENV['USER'], $_ENV['PASSWORD'], $_ENV['DATABASE']);
 
-    //     $this->assertEquals($connection->connect_error, null);
-    // }
+        $this->assertEquals($connection->connect_error, null);
+    }
 }
 ?>
