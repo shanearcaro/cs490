9e059a06727cdab49877ef2bad159c121a95f675
MalcolmS3160 <mcshuler121212@gmail.com>
2022-10-15 20:35:00 -0400

    Added file functionality

--- frontend/login.php
+++ frontend/login.php
@@ -4,13 +4,12 @@
          <title>Login Page</title>
     </head>
     <body>
-        <form name = "loginForm" method = "post" action = "send.php">
+        <form name = "loginForm" method = "post" action = "verifyLogin.php">
             <label for="username">Username:</label><br>
-            <input type="text" id="username" name="username"><br>
+            <input type="text" id="username" name="username" required><br>
             <label for="password">Password:</label><br>
-            <input type="password" id="password" name="password"><br>
+            <input type="password" id="password" name="password" required><br>
             <input type="submit" name="submit" value="Submit">
         </form>
     </body>
-    
 </html>
