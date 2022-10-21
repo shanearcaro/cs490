f2560a8f85ba52068e85e752d53503d0e75f530b
shanearcaro <smarcaro@gmail.com>
2022-10-21 17:15:37 -0400

    Removing these random folders

--- frontend/StudentPages/reviewExam.php
+++
@@ -1,8 +0,0 @@
-<HTML>
-    <head>
-        <Title>Review Exam</Title>
-    </head>
-    <h1>Review Exams</h1>
-    <!-- Display all of the exams that the student can review and let them choose
-    which exam to look at.-->
-</HTML>
