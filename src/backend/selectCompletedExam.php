734204e96e0c08f7526908f902622c5a34b5b374
shanearcaro <smarcaro@gmail.com>
2022-10-23 19:10:08 -0400

    Now getting testcase and case answers

--- src/backend/selectCompletedExam.php
+++ src/backend/selectCompletedExam.php
@@ -26,16 +26,32 @@
     $questionAnswers = array();
     
     // Get all answers for the exam
-    $query = "SELECT ce.studentExamID, ce.questionID, ce.answer, eq.questionPoints FROM CompletedExam AS ce
+    $query = "SELECT ce.studentExamID, ce.questionID, ce.answer, eq.questionPoints, q.testcase1, q.caseAnswer1, q.testcase2, q.caseAnswer2 
+                FROM CompletedExam AS ce
                 INNER JOIN StudentExams AS se ON ce.studentExamID=se.studentExamID
-                INNER JOIN ExamQuestions AS eq on se.examID=eq.examID AND ce.questionID=eq.questionID
-                WHERE ce.studentExamID='{$studentExamID}';";
+                INNER JOIN ExamQuestions AS eq ON se.examID=eq.examID AND ce.questionID=eq.questionID
+                INNER JOIN Questions AS q ON ce.questionID=q.questionID
+                WHERE ce.studentExamID='{$studentExamID}'";
+
     $result = mysqli_query($connection, $query);
     while ($row = mysqli_fetch_array($result)) {
-        $questionID = $row['questionID'];
-        $answer = $row['answer'];
-        $points = $row['questionPoints'];
-        $questionResponse = array('questionID'=>$questionID, 'answer'=>$answer, 'points'=>$points);
+        $questionID     = $row['questionID'];
+        $answer         = $row['answer'];
+        $points         = $row['questionPoints'];
+        $testcase1      = $row['testcase1'];
+        $caseAnswer1    = $row['caseAnswer1'];
+        $testcase2      = $row['testcase2'];
+        $caseAnswer2    = $row['caseAnswer2'];
+
+        $questionResponse = array(
+            'questionID'    => $questionID, 
+            'answer'        => $answer, 
+            'points'        => $points,
+            'testcase1'     => $testcase1,
+            'caseAnswer1'   => $caseAnswer1,
+            'testcase2'     => $testcase2,
+            'caseAnswer2'   => $caseAnswer2,
+        );
         array_push($questionAnswers, $questionResponse);
     }
 
