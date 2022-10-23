8bdcaeab24c99dc2d01831422deda3caef18c01f
shanearcaro <smarcaro@gmail.com>
2022-10-23 19:40:13 -0400

    Successfully writing to python file

--- src/middle/autoGrader.php
+++ src/middle/autoGrader.php
@@ -18,7 +18,37 @@
     // Decode the results of sending the data
     $result = curl_exec($ch);
 
+    // This is a list of questionIDS->answers
+    $questionAnswers = json_decode($result);
+
+    // Loop through every question from the exam
+    for ($i = 0; $i < count($questionAnswers); $i++) {
+        $questionIndex = $questionAnswers[$i];
+        $questionID = $questionIndex->{'questionID'};
+        $answer = $questionIndex->{'answer'} . "\n";
+        $points = $questionIndex->{'points'};
+        $testcase1 = $questionIndex->{'testcase1'};
+        $testcase2 = $questionIndex->{'testcase2'} . "\n";
+
+
+        // This is where the auto grader should run its logic
+        $fileName = 'question.py';
+        $filePath = __DIR__ . DIRECTORY_SEPARATOR . $fileName . "";
+        $file = fopen($filePath, 'w') or die('Unable to open file!');
+
+        // Write shell shebang
+        $shebang = '#!/usr/bin/env python' . "\n";
+        fwrite($file, $shebang);
+        fwrite($file, $answer);
+        fwrite($file, $testcase1);
+        fclose($file);
+
+        // $output = fread($filePath, filesize($file));
+
+    }
+
+
     // Instead of just echoing the data back this is going to have to run the auto grader logic
     curl_close($ch);
-    echo $result;
+    echo $filePath;
 ?>
