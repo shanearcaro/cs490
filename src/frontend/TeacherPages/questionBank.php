7c51ecbd4cdcffd99b45451dc951a661fb8a05e2
shanearcaro <smarcaro@gmail.com>
2022-10-25 14:44:08 -0400

    Teacher question bank page is fully set up

--- src/frontend/TeacherPages/questionBank.php
+++ src/frontend/TeacherPages/questionBank.php
@@ -7,40 +7,84 @@
         echo "<script>alert('Session invalid, logging out.');</script>";
         echo "<script>window.location.href='/';</script>";
         exit();
-        
     }
 ?>
 <HTML>
     <head>
         <Title>Create Question</Title>
-        <style>
-            .questionBox{
-                height: 150px;
-                width: 75%;
-                font-size: 16px;
-            }
         </style>
-        <link rel="Stylesheet" href="../../../style/default.css?<?php echo time();?>"/>
+        <link rel="Stylesheet" href="../../../style/TeacherPages/questionBank.css?<?php echo time();?>"/>
     </head>
-    <h1>Create Question</h1>
     <body onLoad="noBack();">
         <script type="text/javascript">
             window.history.forward();
             function noBack() { window.history.forward(); }
         </script>
-        <form name = "question" method = "post" action = "../post/sendQuestion.php">
-            <label for="questionBox">Enter a Question:</label><br>
-            <textarea type="textarea" id="questionBox" name="questionBox" class="questionBox" required></textarea><br>
-            <label for="testCase1">Enter your first test case:</label><br>
-            <input type="text" id="testCase1" name="testCase1" required><br>
-            <label for="caseAnswer1">Enter the expected answer for test case 1:</label><br>
-            <input type="text" id="caseAnswer1" name="caseAnswer1" required><br>
-            <label for="testCase2">Enter your second test case:</label><br>
-            <input type="text" id="testCase2" name="testCase2" required><br>
-            <label for="caseAnswer2">Enter the expected answer for test case 2:</label><br>
-            <input type="text" id="caseAnswer2" name="caseAnswer2" required><br>
-            <input type="submit" name="submit" value="Submit">
-        </form>
+            <div class="left">
+            <div class="header">
+                <h2 id="title">CS 490</h2>
+                <h3 id="semester">Fall 2022</h3>
+                <img src="../../../assets/njit.png" alt="NJIT LOGO">
+                <h4>Shane Arcaro, Malcolm Shuler, Ege Atay</h4>
+            </div>
+    </div>
+    <div class="right">
+        <div class="examBank">
+            <h2 id="examsTitle">Create Question </h2>
+            <div class="labels">
+                <ul>
+                    <li id="question">Question</li>
+                    <li id="testcase1">Test Case</li>
+                    <li id="answer1">Answer</li>
+                    <li id="testcase2">Test Case</li>
+                    <li id="answer2">Answer</li>
+                </ul>
+            </div>
+            <div class="examRows">
+                <form name="createExam" id="pickExam" method="POST" autocomplete="off" action="../post/sendQuestion.php">
+                    <div class="elements">
+                        <textarea type="textarea" class="question" name="questionBox" class="questionBox" required></textarea>
+                        <input type="text" class="normalElement" name="testCase1" required>
+                        <input type="text" class="normalElement" name="caseAnswer1" required>
+                        <input type="text" class="normalElement" name="testCase2" required>
+                        <input type="text" class="normalElement" name="caseAnswer2" required>
+                    </div>
+                    <div class="examButtons">
+                        <input id="submitButton" form="pickExam" type="submit" name="submit" value="Create">
+                        <input id="backButton" form="backButtonForm" type="submit" name="submit" value="Back">
+                    </div>
+                </form>
+                <form action="http://localhost:8000/src/frontend/TeacherPages/teacher.php" id="backButtonForm"></form>
+            </div>
+        </div>
+    </div>
+            <!-- <div class="form-buttons">
+                <form name = "question" method = "post" action = "../post/sendQuestion.php">
+                    <div class="element">
+                        <label for="questionBox">Enter a Question:</label>
+                        
+                    </div>
+                    <div class="element">
+                        <label for="testCase1">Test case 1:</label>
+                        <input type="text" id="testCase1" name="testCase1" required>
+                    </div>
+                    <div class="element">
+                        <label for="caseAnswer1">Expected Answer:</label>
+                        <input type="text" id="caseAnswer1" name="caseAnswer1" required>
+                    </div>
+                    <div class="element">
+                        <label for="testCase2">Test case 2:</label>
+                        <input type="text" id="testCase2" name="testCase2" required>
+                    </div>
+                    <div class="element">
+                        <label for="caseAnswer2">Expected Answer:</label>
+                        <input type="text" id="caseAnswer2" name="caseAnswer2" required>
+                    </div>
+                    <input type="submit" name="submit" value="Submit">
+                </form>
+            </div> -->
+        </div>
+    </div>
     </body>
     <?php
         include 'teacherBackButton.php';
