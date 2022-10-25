cbf0e76d907a47e3e94168bed5f988caf57059a2
shanearcaro <smarcaro@gmail.com>
2022-10-24 23:07:17 -0400

    Login page is finished

--- index.php
+++ index.php
@@ -6,26 +6,28 @@
     <head>
         <title>Login Page</title>
         <link rel="Stylesheet" href="./style/login.css?<?php echo time();?>"/>
+        <link rel="Left Side" href="./style/include/leftSide.css?<?php echo time();?>"/>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap" rel="stylesheet"> <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link href="https://fonts.googleapis.com/css2?family=PT+Sans+Narrow:wght@400;700&display=swap" rel="stylesheet"> 
+        <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet"> 
     </head>
     <body onLoad="noBack();">
         <script type="text/javascript">
             window.history.forward();
             function noBack() { window.history.forward(); }
         </script>
-        <div class="login">
-            <!-- Need to use send.php to cURL the information instead of standards post -->
-            <form name = "loginForm" method = "post" action = "./src/frontend/post/sendLogin.php">
-                <h1>Login</h1>
-                <label for="username">Username</label><br>
-                <input type="text" id="username" name="username" placeholder="Type your username"><br>
-                <label for="password">Password</label><br>
-                <input type="password" id="password" name="password" placeholder="Type your password"><br>
-                <input type="submit" id="submit" name="submit" value="Submit">
-            </form>
+        <?php include("./src/include/leftSide.php");?>
+        <div class="right">
+            <div class="loginBox">
+                <h2>Log in</h2>
+                <form name = "loginForm" autocomplete="off" method = "post" action = "./src/frontend/post/sendLogin.php">
+                    <label class="label" for="username">Username</label><br>
+                    <input class="input" type="text" id="username" name="username" placeholder="Username" required><br>
+                    <label class="label" for="password">Password</label><br>
+                    <input class="input"type="password" id="password" name="password" placeholder="Password" required><br>
+                    <input type="submit" id="submit" name="submit" value="Login">
+                </form>
+            </div>
         </div>
     </body>
     
--- src/middle/question.py
+++ src/middle/question.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3.9
 def mult(a, b):
     return a * b
-print(mult(7, 5)
+print(mult(13, 2)
 )
